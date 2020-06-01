e419db18899ba8d6590b2e4e9b3162cc9bbe1420
ijk <ijkten@no-email.com>
2020-05-31 22:30:16 -0400

    implemented linked list & have to write tests. Good time to make use of jest snapshot

--- src/data-structures/linked-list.js
+++ src/data-structures/linked-list.js
@@ -29,11 +29,103 @@ class LinkedList {
 
     this.size++;
   }
+
+  insertAt(element, indexAt) {
+    if (indexAt > 0 && indexAt > this.size) {
+      return false;
+    }
+
+    const node = new Node(element);
+    let current = this.head;
+    let previous;
+    let index = 0;
+
+    while (index < indexAt) {
+      index++;
+      previous = current;
+      current = current.next;
+    }
+
+    node.next = current;
+    previous.next = node;
+    this.size++;
+
+    return true;
+  }
+
+  removeFrom(indexAt) {
+    if (indexAt > 0 && indexAt > this.size) {
+      return false;
+    }
+
+    let current = this.head;
+    let previous;
+    let index = 0;
+
+    while (index < indexAt) {
+      index++;
+      previous = current;
+      current = current.next;
+    }
+
+    previous.next = current.next;
+    this.size--;
+
+    return true;
+  }
+
+  remove(element) {
+    let current = this.head;
+    let previous;
+
+    while (current.next) {
+      if (current.data === element) {
+        break;
+      }
+      previous = current;
+      current = current.next;
+    }
+
+    previous.next = current.next;
+    this.size--;
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+
+    while (current.next) {
+      if (current.data === element) {
+        break;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return index;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
 }
 
 const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
+linkedList.add(10);
+linkedList.add(20);
+linkedList.add(30);
+linkedList.insertAt(4, 1);
+
+console.log('index Of 4 ', linkedList.indexOf(4));
 
 console.log(JSON.stringify(linkedList));
+
+linkedList.removeFrom(2);
+
+console.log(JSON.stringify(linkedList));
+
+linkedList.remove(4);
+console.log(JSON.stringify(linkedList));
+
+console.log('is empty?', linkedList.isEmpty());
